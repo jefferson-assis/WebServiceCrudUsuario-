ad154252108d04db267c5fd0085f1bf5a82d6559
jefferson-assis <jeffersondeassis13022000@gmail.com>
2019-09-27 20:26:48 -0300

    cadastrar.php

---
+++ cadastra.php
@@ -0,0 +1,6 @@
+<?php
+	$conexao = mysqli_connect("localhost", "id11034168_root	", "webhost", "id11034168_crudusuario");
+	$nome = $_POST['nome'];
+	$email = $_POST['email'];
+	$senha = $_POST['senha'];
+?>

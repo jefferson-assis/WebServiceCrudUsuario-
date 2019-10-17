007d7514fee704a6ecf3f58c15b8c43c72474a44
jefferson-assis <jeffersondeassis13022000@gmail.com>
2019-10-16 22:36:33 -0300

    atualiza.php

---
+++ atualiza.php
@@ -0,0 +1,14 @@
+<?php
+	$conecta = mysqli_connect();
+
+	$nome = $_POST['nome'];
+	$email = $_POST['email'];
+	$senha = $_POST['senha'];
+	$codigo = $_POST['codigo'];
+
+	$query = "update tb_usuario set nm_usuario = '$nome', ds_email = '$email', ds_senha = '$senha' where cd_usuario = $codigo";
+
+	mysqli_query($conecta,$query);
+
+	echo "Cadastro alterado com sucesso";
+?>
